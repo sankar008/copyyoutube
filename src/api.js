41c6c06906a761a77b425203dce27a015fdb36c7
developer <jordan.digitalwebber1@gmail.com>
2022-08-10 10:29:16 +0530

    add build file

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 export const CURRENCY = "$";
 
-export const IMG_URL = "http://43.205.143.151";
+export const IMG_URL = "http://api.pdf.oyelearn.in";
 
 // ? BASE URL
-export const URL = "http://43.205.143.151:3000/v1";
+export const URL = "http://api.pdf.oyelearn.in:3001/v1";
 
 export const SIGNUP = `${URL}/user`;
 export const USER = `${URL}/user`;
